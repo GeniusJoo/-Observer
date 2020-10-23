f4c7cb0bc3cdc9394770c27adfbe697efaaf133b
Genius_Ju <wnwldnjs1@gmail.com>
2020-10-23 16:16:26 +0900

    Test 입니다.

---
+++ src/test/java/com/roadjava/student/TestClass.java
@@ -0,0 +1,11 @@
+package com.roadjava.student;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("깃 연동 및 소스트리 연동 테스트 클래스 입니다."); // 주지원
+		System.out.println("아아~");
+
+	}
+
+}
